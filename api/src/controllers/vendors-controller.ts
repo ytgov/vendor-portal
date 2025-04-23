161eae6483210194911fdfe2cf613696b8b7c779
caleb <calebburke91@gmail.com>
2025-04-22 18:34:48 -0700

    vendor controller

--- api/src/controllers/vendors-controller.ts
+++ api/src/controllers/vendors-controller.ts
@@ -6,8 +6,8 @@ import BaseController from "@/controllers/base-controller"
 
 import { Vendor } from "@/models"
 import { VendorsPolicy } from "@/policies/vendors-policy"
-import { CreateService, DestroyService, UpdateService } from "@/services/vendors"
 import { IndexSerializer } from "@/serializers/vendors"
+import { VendorSearchService } from "@/services/vendors/vendor-search-service"
 
 export class VendorsController extends BaseController<Vendor> {
   async index() {
@@ -40,7 +40,7 @@ export class VendorsController extends BaseController<Vendor> {
 
   async show() {
     try {
-      const vendor = await this.loadVendor()
+      const vendor = await VendorSearchService.perform(this.params.vendorId)
       if (isNil(vendor)) {
         return this.response.status(404).json({
           message: "Vendor not found",
@@ -61,82 +61,6 @@ export class VendorsController extends BaseController<Vendor> {
         message: `Error fetching vendor: ${error}`,
       })
     }
-  }
-
-  async create() {
-    try {
-      const vendor = await this.buildVendor()
-
-      const policy = this.buildPolicy(vendor)
-      if (!policy.create()) {
-        return this.response
-          .status(403)
-          .json({ message: "You are not authorized to create this vendor." })
-      }
-
-      const permittedAttributes = policy.permitAttributesForCreate(this.request.body)
-      const newVendor = await CreateService.perform(permittedAttributes)
-      return this.response.status(201).json({ vendor: newVendor })
-    } catch (error) {
-      logger.error(`Vendor creation failed: ${error}`, { error })
-      return this.response.status(422).json({ message: `Vendor creation failed: ${error}` })
-    }
-  }
-
-  async update() {
-    try {
-      const vendor = await this.loadVendor()
-
-      if (isNil(vendor)) {
-        return this.response.status(404).json({
-          message: `Vendor not found`,
-        })
-      }
-
-      const policy = this.buildPolicy(vendor)
-      if (!policy.update()) {
-        return this.response
-          .status(403)
-          .json({ message: "You are not authorized to update this vendor." })
-      }
-
-      const permitAttributes = policy.permitAttributes(this.request.body)
-      const newVendor = await UpdateService.perform(vendor, permitAttributes)
-      return this.response.json({ vendor: newVendor })
-    } catch (error) {
-      logger.error(`Vendor update failed: ${error}`, { error })
-      return this.response.status(422).json({ message: `Vendor update failed: ${error}` })
-    }
-  }
-
-  async destroy() {
-    try {
-      const vendor = await this.loadVendor()
-      if (isNil(vendor)) {
-        return this.response.status(404).json({ message: "Vendor not found." })
-      }
-
-      const policy = this.buildPolicy(vendor)
-      if (!policy.destroy()) {
-        return this.response
-          .status(403)
-          .json({ message: "You are not authorized to destroy this vendor." })
-      }
-
-      await DestroyService.perform(vendor, this.currentUser)
-      return this.response.status(204).json({ message: "Vendor was deleted" })
-    } catch (error) {
-      logger.error(`Vendor deletion failed: ${error}`, { error })
-      return this.response.status(422).json({ message: `Vendor deletion failed: ${error}` })
-    }
-  }
-
-  private async buildVendor() {
-    return Vendor.build(this.request.body)
-  }
-
-  private async loadVendor(): Promise<Vendor | null> {
-    return Vendor.findByPk(this.params.vendorId)
   }
 
   private buildPolicy(vendor: Vendor) {
