35d09a5cb5ba31b60d60800e6c25d65944726d64
caleb <calebburke91@gmail.com>
2025-04-28 12:35:04 -0700

    Export vendor search service

--- api/src/services/vendors/index.ts
+++ api/src/services/vendors/index.ts
@@ -1,3 +1,4 @@
 export { CreateService } from "./create-service"
 export { DestroyService } from "./destroy-service"
 export { UpdateService } from "./update-service"
+export { VendorSearchService } from "./vendor-search-service"
