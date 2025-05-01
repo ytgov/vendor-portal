693f1730131eb276b958728c0f723814223e074a
caleb <calebburke91@gmail.com>
2025-05-01 15:38:20 -0700

    Using new pattern

--- web/src/components/vendor-programs/VendorProgramPendingList.vue
+++ web/src/components/vendor-programs/VendorProgramPendingList.vue
@@ -1,6 +1,6 @@
 <template>
   <v-skeleton-loader
-    v-if="isNil(programs)"
+    v-if="isNil(programs) || isLoading"
     type="card"
   />
   <div v-else>
@@ -20,30 +20,26 @@
 
 <script setup lang="ts">
 import { isNil } from "lodash"
-import { ref, watch } from "vue"
+import { computed, ref } from "vue"
 
 import { formatDate } from "@/utils/formatters"
 
-import programsApi, { Program, ProgramQueryOptions } from "@/api/programs-api"
-
 import useVendor from "@/use/use-vendor"
+import usePrograms from "@/use/use-programs"
 
 const props = defineProps<{ vendorId: string }>()
 const vendorId = ref(props.vendorId)
 const { vendor } = useVendor(vendorId)
 
-const programs = ref<Program[] | null>(null)
+const query = computed(() => {
+  if (isNil(vendor.value)) return {}
 
-watch(vendor, async () => {
-  if (!isNil(vendor.value)) {
-    const query: ProgramQueryOptions = {
-      filters: {
-        withPendingVendor: vendor.value.id,
-      },
-    }
-
-    const data = await programsApi.list(query)
-    programs.value = data.programs
+  return {
+    filters: {
+      withPendingVendor: vendor.value.id,
+    },
   }
 })
+
+const { programs, isLoading } = usePrograms(query, { skipWatchIf: () => isNil(vendor.value) })
 </script>
