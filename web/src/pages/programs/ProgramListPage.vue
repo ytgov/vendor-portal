d148ea435b0f67f5a1627dcbee414074e64567af
caleb <calebburke91@gmail.com>
2025-05-01 11:33:24 -0700

    List programs, doesnt look right currently

--- web/src/pages/programs/ProgramListPage.vue
+++ web/src/pages/programs/ProgramListPage.vue
@@ -1,16 +1,29 @@
 <template>
-  <v-row>
-    <v-col
-      cols="12"
-      md="6"
-    >
-      <PSLRProgramInfoCard :show-apply="true" />
-    </v-col>
-  </v-row>
+  <div
+    v-for="(program, index) in programs"
+    :key="index"
+  >
+    <v-row>
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <PSLRProgramInfoCard
+          :program-id="`${program.id}`"
+          :show-apply="true"
+        />
+      </v-col>
+    </v-row>
+  </div>
 </template>
-<script setup lang="ts">
-import PSLRProgramInfoCard from "@/components/programs/ecdev-pslr/ProgramInfoCard.vue"
-import useBreadcrumbs, { BASE_CRUMB } from "@/use/use-breadcrumbs"
 
-useBreadcrumbs("Programs available in this portal ", [BASE_CRUMB])
+<script setup lang="ts">
+import PSLRProgramInfoCard from "@/components/programs/ProgramInfoCard.vue"
+
+import useBreadcrumbs from "@/use/use-breadcrumbs"
+import usePrograms from "@/use/use-programs"
+
+const { programs } = usePrograms()
+
+useBreadcrumbs("", [{ title: "Programs available in this portal ", to: "/programs" }])
 </script>
