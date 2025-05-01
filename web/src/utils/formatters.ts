21dcb2f01a9d45488c5862954cc5ae70edbf6987
caleb <calebburke91@gmail.com>
2025-05-01 11:24:04 -0700

    updated fromatter

--- web/src/utils/formatters.ts
+++ web/src/utils/formatters.ts
@@ -1,10 +1,31 @@
-import { isEmpty } from "lodash"
+import { isEmpty, isString } from "lodash"
 import { DateTime } from "luxon"
 
+// ISO 8601 to YYY-MM-DD
 export function formatDate(input: string | Date | undefined): string {
   if (isEmpty(input)) return ""
   if (typeof input == "string") return DateTime.fromISO(input).toLocal().toFormat("yyyy-MM-dd")
   if (input) return DateTime.fromJSDate(input).toLocal().toFormat("yyyy-MM-dd")
+
+  return ""
+}
+
+// ISO 8601 to November 2, 2023
+export function formatDateHumanReadable(input: string | Date | undefined): string {
+  if (isEmpty(input)) return ""
+
+  if (isString(input)) {
+    const date = new Date(input)
+    const options: Intl.DateTimeFormatOptions = { year: "numeric", month: "long", day: "numeric" }
+    return date.toLocaleDateString("en-US", options)
+  } else if (input) {
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }
+    return input.toLocaleDateString("en-US", options)
+  }
 
   return ""
 }
