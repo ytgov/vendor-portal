0fcf390b81106358103467a2577d0bf62bfcd2a2
caleb <calebburke91@gmail.com>
2025-05-01 11:29:08 -0700

    Documentations create page

---
+++ web/src/pages/documentations/DocumentationCreatePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card>
+    <v-card-title class="d-flex flex-md-row">
+      Create Documentation
+      <v-spacer />
+      <v-btn
+        :to="{ name: 'administration/DocumentationsPage' }"
+        color="primary"
+        variant="outlined"
+        text="Back"
+      />
+    </v-card-title>
+    <v-card-text>
+      <DocumentationCreateForm class="mt-10" />
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import DocumentationCreateForm from "@/components/documentations/DocumentationCreateForm.vue"
+</script>
---
+++ web/src/pages/documentations/DocumentationsManagePage.vue
@@ -0,0 +1,117 @@
+<template>
+  <v-skeleton-loader
+    v-if="isNil(documentations)"
+    type="table"
+  />
+  <v-card>
+    <v-card-text>
+      <div class="d-flex">
+        <v-text-field
+          v-model="search"
+          label="Search"
+        />
+        <v-btn
+          color="primary"
+          class="ml-4"
+          :to="{ name: 'administration/DocumentationCreatePage' }"
+          style="height: 40px"
+          text="Create Documentation"
+        />
+      </div>
+      <v-data-table-server
+        v-model:items-per-page="perPage"
+        :page="page"
+        :headers="headers"
+        :search="search"
+        :items="documentations"
+        :items-length="totalCount"
+        :loading="isLoading"
+        style="border: 1px #ccc solid; border-radius: 3px"
+        @update:page="updatePage"
+      >
+      </v-data-table-server>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { isEmpty, isNil } from "lodash"
+import { computed, ref } from "vue"
+import { useRoute, useRouter } from "vue-router"
+
+import useBreadcrumbs, { ADMIN_CRUMB } from "@/use/use-breadcrumbs"
+import useDocumentations, {
+  DocumentationFiltersOptions,
+  DocumentationQueryOptions,
+} from "@/use/use-documentations"
+import { useRouteQuery } from "@vueuse/router"
+
+const headers = ref([
+  { title: "Name", key: "name" },
+  { title: "Department", key: "department" },
+  { title: "Offered By", key: "offeredBy" },
+  { title: "Start Date", key: "startDate" },
+  { title: "End Date", key: "endDate" },
+])
+
+const search = ref("")
+
+const route = useRoute()
+const router = useRouter()
+
+const page = useRouteQuery<number>("page", 1, {
+  route,
+  router,
+  transform: {
+    get: (value: number) => value,
+    set: (value: number) => value,
+  },
+})
+
+const perPage = useRouteQuery<number>("perPage", 10, {
+  route,
+  router,
+  transform: {
+    get: (value: number) => value,
+    set: (value: number) => value,
+  },
+})
+
+const searchFilter = computed<Pick<DocumentationFiltersOptions, "search">>(() => {
+  if (isEmpty(search.value)) {
+    return {}
+  }
+
+  return {
+    search: `${search.value}`,
+  }
+})
+
+const filters = computed<DocumentationFiltersOptions>(() => {
+  return {
+    ...searchFilter.value,
+  }
+})
+
+const documentationsQuery = computed<DocumentationQueryOptions>(() => {
+  return {
+    filters: filters.value,
+    perPage: perPage.value,
+    page: page.value,
+  }
+})
+
+const { documentations, totalCount, isLoading, refresh } = useDocumentations(documentationsQuery)
+
+useBreadcrumbs("Manage Documentations", [ADMIN_CRUMB])
+
+// Necessary to avoid wiping page value due to bug in Vuetify table implementation
+// which causes page value to be wiped if changed during loading state.
+function updatePage(newPage: number) {
+  if (isLoading.value) return
+
+  page.value = newPage
+}
+
+defineExpose({ refresh })
+</script>
