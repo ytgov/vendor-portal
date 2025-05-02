60ba09e2d08ca58e3d761d0d65478ecd1f458989
caleb <calebburke91@gmail.com>
2025-05-02 10:36:56 -0700

    Some chips

---
+++ web/src/components/vendors/VendorChip.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-skeleton-loader
+    v-if="isNil(vendor) || isLoading"
+    type="chip"
+  />
+  <div v-else>
+    {{ vendor.name }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { toRefs } from "vue"
+import { isNil } from "lodash"
+
+import useVendor from "@/use/use-vendor"
+
+const props = defineProps<{
+  vendorId: number
+}>()
+
+const { vendorId } = toRefs(props)
+const { vendor, isLoading } = useVendor(vendorId)
+</script>
