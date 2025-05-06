ee6ef93f987d8cdb779db3af9859ad42854d3100
caleb <calebburke91@gmail.com>
2025-05-05 19:06:56 -0700

    Beefing up the vendor users

--- api/src/policies/vendor-users-policy.ts
+++ api/src/policies/vendor-users-policy.ts
@@ -2,11 +2,15 @@ import { Attributes, FindOptions } from "@sequelize/core"
 
 import { Path } from "@/utils/deep-pick"
 import { VendorUser, User } from "@/models"
-import { ALL_RECORDS_SCOPE, NO_RECORDS_SCOPE, PolicyFactory } from "@/policies/base-policy"
+import { ALL_RECORDS_SCOPE, PolicyFactory } from "@/policies/base-policy"
 
 export class VendorUsersPolicy extends PolicyFactory(VendorUser) {
   show(): boolean {
     if (this.user.isSystemAdmin) {
+      return true
+    }
+
+    if (this.record.userId == this.user.id) {
       return true
     }
 
@@ -50,6 +54,10 @@ export class VendorUsersPolicy extends PolicyFactory(VendorUser) {
       return ALL_RECORDS_SCOPE
     }
 
-    return NO_RECORDS_SCOPE
+    return {
+      where: {
+        userId: user.id,
+      },
+    }
   }
 }
--- api/src/serializers/vendor-users/show-serializer.ts
+++ api/src/serializers/vendor-users/show-serializer.ts
@@ -1,16 +1,41 @@
-import { pick } from "lodash"
+import { isUndefined, pick } from "lodash"
 
 import { VendorUser } from "@/models"
 
 import BaseSerializer from "@/serializers/base-serializer"
+import { UserShowView } from "@/serializers/users/show-serializer"
+import { VendorShowView } from "@/serializers/vendors/show-serializer"
 
 export type VendorUserShowView = Pick<
   VendorUser,
-  "vendorId" | "userId" | "isActive" | "isAdmin" | "decisionByUserId" | "decisionAt"
->
+  | "vendorId"
+  | "userId"
+  | "isActive"
+  | "isAdmin"
+  | "decisionByUserId"
+  | "decisionAt"
+  | "updatedAt"
+  | "createdAt"
+> & {
+  vendor?: VendorShowView
+  user?: UserShowView
+  decisionByUser?: UserShowView
+}
 
 export class ShowSerializer extends BaseSerializer<VendorUser> {
   perform(): VendorUserShowView {
+    if (isUndefined(this.record.vendor)) {
+      throw new Error("Vendor must be eager loaded for detailed view")
+    }
+
+    if (isUndefined(this.record.user)) {
+      throw new Error("User must be eager loaded for detailed view")
+    }
+
+    if (isUndefined(this.record.decisionByUser)) {
+      throw new Error("Decision By User must be eager loaded for detailed view")
+    }
+
     return {
       ...pick(this.record, [
         "vendorId",
@@ -19,6 +44,8 @@ export class ShowSerializer extends BaseSerializer<VendorUser> {
         "isAdmin",
         "decisionByUserId",
         "decisionAt",
+        "updatedAt",
+        "createdAt",
       ]),
     }
   }
