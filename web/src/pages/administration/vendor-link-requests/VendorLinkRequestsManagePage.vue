c3041f2a60b8b13c57780ab679a60b00911b5002
caleb <calebburke91@gmail.com>
2025-05-09 14:21:30 -0700

    New routes

---
+++ web/src/pages/administration/vendor-link-requests/VendorLinkRequestsManagePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <VendorLinkRequestsDataTableServer @clicked="goToVendorLinkRequestPage" />
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from "vue-router"
+
+import useBreadcrumbs from "@/use/use-breadcrumbs"
+
+import VendorLinkRequestsDataTableServer from "@/components/vendor-link-requests/VendorLinkRequestsDataTableServer.vue"
+
+const router = useRouter()
+
+function goToVendorLinkRequestPage(vendorLinkRequestId: number) {
+  router.push({ name: "administration/VendorLinkRequestPage", params: { vendorLinkRequestId } })
+}
+
+useBreadcrumbs("Manage Vendor Link Requests", [
+  { title: "Vendor Link Requests", to: { name: "administration/VendorLinkRequestsManagePage" } },
+])
+</script>
