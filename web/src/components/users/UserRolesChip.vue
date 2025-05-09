6cf467e9a5924287e9717b6f15a0cdd1c8d3b59e
caleb <calebburke91@gmail.com>
2025-05-09 14:52:46 -0700

    New patterns changes

---
+++ web/src/components/users/UserRolesChip.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-chip
+      v-for="(role, index) of roles"
+      :key="index"
+      :color="getRoleColor(role)"
+      class="my-0"
+      :text="role"
+      size="small"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { UserRoles } from "@/api/users-api"
+
+defineProps<{ roles: UserRoles[] }>()
+
+function getRoleColor(role: UserRoles) {
+  switch (role) {
+    case UserRoles.USER:
+      return "grey"
+    case UserRoles.SYSTEM_ADMIN:
+      return "blue"
+    case UserRoles.PROGRAM_ADMIN:
+      return "blue"
+    default:
+      return "grey"
+  }
+}
+</script>
