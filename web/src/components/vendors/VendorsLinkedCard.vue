a4cd148d7fb378307a307c500d5d05e9f471a987
caleb <calebburke91@gmail.com>
2025-05-12 12:57:30 -0700

    No linked vendors feedback

--- web/src/components/vendors/VendorsLinkedCard.vue
+++ web/src/components/vendors/VendorsLinkedCard.vue
@@ -5,9 +5,13 @@
   >
     <VendorLinkCard :vendor-id="vendor.id" />
   </div>
+  <div v-if="isEmpty(vendors)">
+    <p>No linked vendors</p>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { isEmpty } from "lodash"
 import { computed } from "vue"
 
 import useVendors, { VendorQueryOptions } from "@/use/use-vendors"
