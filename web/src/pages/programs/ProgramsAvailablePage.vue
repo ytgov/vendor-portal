c003b783a129ff94d03b5d5b297647af10e40d19
caleb <calebburke91@gmail.com>
2025-05-12 12:46:46 -0700

    Dont show programs if user is not in a vendor

--- web/src/pages/programs/ProgramsAvailablePage.vue
+++ web/src/pages/programs/ProgramsAvailablePage.vue
@@ -1,6 +1,10 @@
 <template>
+  <div v-if="isEmpty(vendorUsers)">
+    <p>No programs available</p>
+  </div>
   <div
     v-for="(program, index) in programs"
+    v-else
     :key="index"
   >
     <v-row>
@@ -18,14 +22,23 @@
 </template>
 
 <script setup lang="ts">
+import { isEmpty } from "lodash"
+
 import useBreadcrumbs from "@/use/use-breadcrumbs"
 import usePrograms from "@/use/use-programs"
+import useVendorUsers from "@/use/use-vendor-users"
 
 import ProgramInfoCard from "@/components/programs/ProgramInfoCard.vue"
 
 const { programs } = usePrograms()
+const { vendorUsers } = useVendorUsers()
 
 useBreadcrumbs("Available Programs", [
-  { title: "Programs available in this portal ", to: "/programs" },
+  {
+    title: "Programs available in this portal ",
+    to: {
+      name: "programs/ProgramsAvailablePage",
+    },
+  },
 ])
 </script>
