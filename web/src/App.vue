64f16c80d996418e37db079905ec4c2bfd3a650b
caleb <calebburke91@gmail.com>
2025-05-13 16:25:28 -0700

    App error catcher

--- web/src/App.vue
+++ web/src/App.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, ref, watch } from "vue"
+import { computed, onErrorCaptured, ref, watch } from "vue"
 import { useAuth0 } from "@auth0/auth0-vue"
 import { useRoute, useRouter } from "vue-router"
 
@@ -30,6 +30,7 @@ import useCurrentUser from "@/use/use-current-user"
 
 import PageLoader from "@/components/common/PageLoader.vue"
 import AppSnackbar from "@/components/common/AppSnackbar.vue"
+import ApiError from "./api/api-error"
 
 const route = useRoute()
 const isUnauthenticatedRoute = computed(() => route.meta.requiresAuth === false)
@@ -72,4 +73,35 @@ watch(
   },
   { immediate: true }
 )
+
+onErrorCaptured((error: Error) => {
+  if (error instanceof ApiError) {
+    redirectToAppropriateErrorPage(error.status)
+  } else {
+    console.error(`Unhandled error: ${error}`, { error })
+  }
+})
+
+function redirectToAppropriateErrorPage(httpErrorCode: number) {
+  if (httpErrorCode === 400 || httpErrorCode === 422) return
+
+  switch (httpErrorCode) {
+    case 401:
+      return router.replace({
+        name: "errors/UnauthorizedPage",
+      })
+    case 403:
+      return router.replace({
+        name: "errors/ForbiddenPage",
+      })
+    case 404:
+      return router.replace({
+        name: "errors/NotFoundPage",
+      })
+    default:
+      return router.replace({
+        name: "errors/InternalServerErrorPage",
+      })
+  }
+}
 </script>
