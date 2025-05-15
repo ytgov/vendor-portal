0d4ffb0f07dfc3b38d2bd1d083355ee12095cd66
caleb <calebburke91@gmail.com>
2025-05-15 12:04:50 -0700

    Serializer updated to handle model change

--- api/src/serializers/documentations/show-serializer.ts
+++ api/src/serializers/documentations/show-serializer.ts
@@ -6,13 +6,21 @@ import BaseSerializer from "@/serializers/base-serializer"
 
 export type DocumentationShowView = Pick<
   Documentation,
-  "id" | "name" | "description" | "format" | "updatedAt" | "createdAt"
+  "id" | "name" | "description" | "format" | "expires" | "updatedAt" | "createdAt"
 >
 
 export class ShowSerializer extends BaseSerializer<Documentation> {
   perform(): DocumentationShowView {
     return {
-      ...pick(this.record, ["id", "name", "description", "format", "updatedAt", "createdAt"]),
+      ...pick(this.record, [
+        "id",
+        "name",
+        "description",
+        "format",
+        "expires",
+        "updatedAt",
+        "createdAt",
+      ]),
     }
   }
 }
