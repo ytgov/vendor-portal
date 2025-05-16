069d166861ba0eaeb2aca56835c1b538c6fa32f7
caleb <calebburke91@gmail.com>
2025-05-16 14:22:43 -0700

    Description in model

--- api/src/db/seeds/development/002.seed-programs.ts
+++ api/src/db/seeds/development/002.seed-programs.ts
@@ -11,6 +11,22 @@ export async function seed(): Promise<void> {
       offeredBy: "Yukon",
       isActive: true,
       name: "Paid Sick Leave Rebate",
+      description: `<p class="mb-3">
+    The Paid Sick Leave Rebate provides up to 40 hours of wages for eligible workers who are sick
+    with illness (or injury not covered by any other act, benefit or program).
+  </p>
+  <p class="mb-3">
+    This program helps Yukoners prioritize their health and reduce the spread of illness in the
+    workplace.
+  </p>
+  <p class="mb-3">
+    For more information, visit
+    <a
+      href="https://yukon.ca/en/health-and-wellness/covid-19-information/economic-and-social-supports-covid-19/paid-sick-leave"
+      target="_blank"
+      >Yukon.ca</a
+    >.
+  </p>`,
     },
     {
       slug: "test-program",
@@ -18,6 +34,7 @@ export async function seed(): Promise<void> {
       offeredBy: "None",
       isActive: true,
       name: "Testing program",
+      description: "<p>Test program description</p>",
     },
   ]
 
